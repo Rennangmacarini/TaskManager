ede1224a86872b6e0ae808d982cb80500db6359d
Rennangmacarini <rennangmacarini@gmail.com>
2025-09-13 17:46:31 -0300

    refactor: ajusting page task details

--- src/Pages/task-details.jsx
+++ src/Pages/task-details.jsx
@@ -8,7 +8,7 @@ const TaskDetailsPage = () => {
     useEffect(() => {
         const fetchTask = async () => {
             const response = await fetch(
-                `http://localhost:3000/task/${taskId}`,
+                `http://localhost:3000/tasks/${taskId}`,
                 {
                     method: "GET",
                 }
@@ -22,7 +22,7 @@ const TaskDetailsPage = () => {
     return (
         <div>
             <h1>{task?.title}</h1>
-            <h1>{task?.description}</h1>
+            <p>{task?.description}</p>
         </div>
     )
 }
